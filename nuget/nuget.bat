73c33a3ffd3251d0255d58d65284fa2d8912511c
Dale Newman <dalenewman@gmail.com>
2020-04-22 16:36:36 -0400

    added batch file for pushing to nuget

---
+++ nuget/nuget.bat
@@ -0,0 +1,6 @@
+REM right click on project and choose Pack to pack
+
+nuget push "c:\temp\modules\Transformalize.Provider.CsvHelper.0.6.31-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.CsvHelper.Autofac.0.6.31-beta.nupkg" -source https://api.nuget.org/v3/index.json
+
+
