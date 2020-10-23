4a377fd716e2a481570753469e7250065c2adeb1
Dale Newman <dalenewman@gmail.com>
2020-10-22 22:07:14 -0400

    Update Dependencies

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,7 +1,7 @@
 nuget pack Transformalize.Provider.CsvHelper.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.CsvHelper.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.CsvHelper.0.8.18-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.CsvHelper.Autofac.0.8.18-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.CsvHelper.0.8.29-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.CsvHelper.Autofac.0.8.29-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
